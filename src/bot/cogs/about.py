4f760c0bf53cd50e91f92fbb4071178133b0d54f
dbchristenson <dbchristenson@uchicago.edu>
2023-12-21 16:56:40 +0800

    restoring file

---
+++ src/bot/cogs/about.py
@@ -0,0 +1,27 @@
+import nextcord
+from nextcord.ext import commands
+
+
+# Views
+class AboutView(nextcord.ui.View):
+    '''A view that provides info about the bot'''
+
+    def __init__(self):
+        super().__init__(timeout=None)
+
+
+# Cog
+class About(commands.Cog):
+    def __init__(self, bot: commands.Bot):
+        self.bot = bot
+
+    @commands.command()
+    async def about(self, ctx: commands.Context):
+        '''Shows info about the bot'''
+        await ctx.message.delete()
+
+
+# Run
+def setup(bot: commands.Bot):
+    bot.add_cog(About(bot))
+    return
